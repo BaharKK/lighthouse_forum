3a0b0e9220d2c97319b866d32368edbdc2af0c29
BaharKK <bahar_rachapalli@yahoo.com>
2016-02-02 08:42:30 +0000

    refactoring static individual post routes to dynamic posts#show

--- config/routes.rb
+++ config/routes.rb
@@ -54,10 +54,8 @@ LighthouseForumCd::Application.routes.draw do
   #     resources :products
   #   end
     get 'posts', to: 'posts#index'
-    get 'posts/0', to: 'posts#post0'
-    get 'posts/1', to: 'posts#post1'
-    get 'posts/2', to: 'posts#post2'
-    get 'posts/3', to: 'posts#post3'
+    get 'posts/:id', to: 'posts#show'
+    
 
     #[tons of helpful comments]
 end
